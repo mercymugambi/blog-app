4d4722ce99cb3f9be4dba49015bfa914db228177
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-13 19:48:33 +0300

    Create likes table

---
+++ db/migrate/20230713164806_add_index_to_likes_on_author_id.rb
@@ -0,0 +1,4 @@
+class AddIndexToLikesOnAuthorId < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_13_111850) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_164514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,20 +23,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_13_111850) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "user_id"
+    t.integer "author_id"
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "author_id"
+    t.integer "author_id"
     t.string "title"
     t.string "text"
-    t.string "comments_counter"
+    t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_13_111850) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users", column: "author_id"
 end
