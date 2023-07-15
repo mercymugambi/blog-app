53ad2dc87a7893ecc9781f72a278d114db265fe9
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-15 19:04:01 +0300

    Generate count for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,19 @@
 class Post < ApplicationRecord
     has_many :comments, foreign_key: 'post_id'
     has_many :likes, foreign_key: 'post_id'
-    belongs_to :user, foreign_key: 'author_id'
+    belongs_to :user, foreign_key: 'author_id', counter_cache: true
+
+    after_create :increment_user_posts_counter
+    after_destroy :decrement_user_posts_counter
+
+    private
+
+    def increment_user_posts_counter
+        user.increment!(:posts_counter)
+    end
+
+    def decrement_user_posts_counter
+        user.decrement!(:posts_counter)
+    end
 end
   
