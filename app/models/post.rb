e84b2ee53db06e7e574fc3bc0cae634b72a43532
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-15 20:22:57 +0300

    Validate title, comments counter and likes counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   belongs_to :user, foreign_key: 'author_id', counter_cache: true
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :update_posts
+
   after_create :increment_user_posts_counter
   after_destroy :decrement_user_posts_counter
 
