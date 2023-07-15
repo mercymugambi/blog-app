cfbea7a5f02536d1ee93ec229ac365913a7a4926
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-15 17:10:25 +0300

    Create a comment for user john post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,5 @@
 class Comment < ApplicationRecord
-    class Comment < ApplicationRecord
-        belongs_to :post, foreign_key: 'post_id'
-        belongs_to :user, foreign_key: 'author_id'
-      end
+    belongs_to :post, foreign_key: 'post_id'
+    belongs_to :user, foreign_key: 'author_id'
   end
   
