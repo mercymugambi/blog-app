8c502f4288aa2700b232d053393c314e8b982ca7
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-15 16:12:39 +0300

    Add users association on the comments, likes and posts table

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+    belongs_to :post
+    belongs_to :author, class_name: 'User'
+    belongs_to :user, foreign_key: 'post_id'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
 
-  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'User'
   belongs_to :user, foreign_key: 'author_id'
 end
