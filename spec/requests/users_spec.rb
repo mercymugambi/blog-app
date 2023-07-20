06075bce1bee41a250761318951894e0fb1e0106
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-20 16:10:53 +0300

    Write rspec test for the user

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,9 @@
+require 'rails_helper'
+
 describe "GET /show" do
-    it "renders a successful response" do
-        user =User.create! Valid_attributes
-        get user_url(user)
-        expect(response).to be_successful
-    end
+  it "renders a successful response" do
+    user = User.create!(name: "Mercy", photo: "https://unsplash.com/photos/F_-0BxGuVvo", bio: "Lecturer")
+    get user_url(user)
+    expect(response).to be_successful
+  end
 end
