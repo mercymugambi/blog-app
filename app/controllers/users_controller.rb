d0900f16187e70a3f5ad3e267f0902f39eb1e11c
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-22 01:49:56 +0300

    Fix rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   # GET /users or users.json
-  def index; 
-  @users = User.all
+  def index
+    @users = User.all
   end
 
-  def show; 
-  @user = User.find(params[:id])
-  @most_recent_posts = @user.most_recent_posts
-  @user_posts = @user.posts
+  def show
+    @user = User.find(params[:id])
+    @most_recent_posts = @user.most_recent_posts
+    @user_posts = @user.posts
   end
 end
