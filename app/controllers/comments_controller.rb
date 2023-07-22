24a1b50aa0c7f94eee360d3ee1241dc701fdc615
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-22 16:35:31 +0300

    Add comment to each post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,27 @@
-# app/controllers/comments_controller.rb
 class CommentsController < ApplicationController
+    before_action :set_user_and_post
+  
+    def new
+      @comment = Comment.new
+    end
+  
     def create
-      @user = User.find(params[:user_id])
-      @post = Post.find(params[:post_id])
       @comment = @post.comments.build(comment_params)
-      @comment.user = @user
+      @comment.user = current_user
   
       if @comment.save
         redirect_to user_post_path(@user, @post), notice: 'Comment was successfully created.'
       else
-        redirect_to user_post_path(@user, @post), alert: 'Failed to create comment.'
+        render :new
       end
     end
   
     private
+  
+    def set_user_and_post
+      @user = User.find(params[:user_id])
+      @post = @user.posts.find(params[:post_id])
+    end
   
     def comment_params
       params.require(:comment).permit(:text)
