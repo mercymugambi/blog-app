f046aea3bb5a76214c98c036cbad32e496b37005
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-25 17:24:15 +0300

    Configure Capybara on rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'capybara/rspec'
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -32,6 +34,10 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{Rails.root}/spec/fixtures"
+
+  config.include Capybara::DSL, type: :feature
+  Capybara.default_driver = :selenium_chrome
+  Capybara.javascript_driver = :selenium_chrome 
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
