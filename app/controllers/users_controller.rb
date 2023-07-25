14d701884406efcc1eec41764baf21b5faabe88e
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-25 17:14:16 +0300

    Optimize N+1 Query for Posts in show Action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   # GET /users or users.json
   def index
-    @users = User.all
+    @all_users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(posts: [:user, comments: :user]).find(params[:id])
     @most_recent_posts = @user.most_recent_posts
     @user_posts = @user.posts
   end
