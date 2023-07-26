a4664dff00cd2d1ecfd3fd6ba1d59fa65aade2d9
Ndonyi Maurice Djam <93107716+citec-47@users.noreply.github.com>
2023-07-26 07:09:13 -0700

    Update posts_show_spec.rb
    Posts_show

---
+++ spec/features/posts_show_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+require 'capybara/rspec'
+RSpec.describe Post, type: :feature do
+  user = User.create(name: 'Tom', posts_counter: 3, photo: 'https://pic.com',
+                     bio: 'Project manager')
+  subject do
+    Post.new(author_id: user.id, title: 'Post 2', text: 'Text for Post 2', comments_counter: 2, likes_counter: 2)
+  end
+  before { subject.save }
+  describe 'Post show page' do
+    it "I can see a post's title." do
+      visit user_post_path(user.id, subject.id)
+      page.has_content?(subject.title)
+    end
+    it 'I can see who wrote the post.' do
+      visit user_post_path(user.id, subject.id)
+      page.has_content?(user.name)
+    end
+    it 'I can see how many comments it has.' do
+      visit user_post_path(user.id, subject.id)
+      page.has_content?(subject.comments_counter)
+    end
+    it 'I can see how many likes it has.' do
+      visit user_post_path(user.id, subject.id)
+      page.has_content?(subject.likes_counter)
+    end
+    it 'I can see the post body.' do
+      visit user_post_path(user.id, subject.id)
+      page.has_content?(subject.text)
+    end
+    it 'I can see the username of each commentor.' do
+      comment = Comment.new(author_id: user.id, post_id: subject.id, text: 'I like it')
+      visit user_post_path(user.id, subject.id)
+      page.has_content?(comment.user.name)
+    end
+    it 'I can see the comment each commentor left. ' do
+      comment = Comment.new(author_id: user.id, post_id: subject.id, text: 'I like it')
+      visit user_post_path(user.id, subject.id)
+      page.has_content?(comment.text)
+    end
+  end
+end
