3767886b702038bf14f4e99bc87ec83bebf1a21b
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-26 16:34:09 +0300

    Fix posts show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   before_action :set_user, only: %i[index show new create]
-  
+
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:user, comments: :user).paginate(page: params[:page], per_page: 5)
+    @posts = @user.posts.includes(:user, comments: :user).order(created_at: :desc).paginate(page: params[:page], per_page: 5)
   end
 
   def show
