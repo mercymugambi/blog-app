f2c553ec3c1869c1d0352d32e7c83d1122b94644
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-27 21:22:20 +0300

    Fix Linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
   protected
+
   def configure_permitted_parameters
     attributes = [:name]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
