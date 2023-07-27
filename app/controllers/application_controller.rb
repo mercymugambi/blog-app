609e42b054ccb06ae8a548ee55e75f0c1190e424
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-27 17:44:07 +0300

    Add configuration for the sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def current_user
-    @current_user ||= User.first
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  protected
+  def configure_permitted_parameters
+    attributes = [:name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
-
-  helper_method :current_user
 end
